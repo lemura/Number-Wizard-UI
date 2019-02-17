510572e3b6a8ac12f4c2c028d62ed64620b27d92
lemura <33998688+lemura@users.noreply.github.com>
2019-02-17 22:22:12 +0300

    Add files via upload
    Scripts for my game number wizard

---
+++ MusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    static MusicPlayer instance = null;
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("duplicate object destroing");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start () {
+        Debug.Log("Music Player Start " + GetInstanceID());
+       
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+    public int maxGuessesAllowed = 10; /* это значение переписывается значением указанным в редакторе */
+    public Text text;
+
+    void Start () {
+        StartGame();
+	}
+    
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        NextGuess();
+    }
+	    
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            GuessHigher();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            GuessLower();
+        }
+    }
+
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+    public void GuessLower()  {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess() {
+        guess = Random.Range(min, max);
+        text.text = guess.ToString();
+        maxGuessesAllowed = maxGuessesAllowed - 1;
+        if (maxGuessesAllowed <= 0)
+        {
+           SceneManager.LoadScene("Win");
+        }
+    }
+}
